6759e92f7516be2860a66a099debe7565160c8bf
root <root@4ab4cae2f808>
2024-11-30 23:23:23 +0000

    Implemntes sceleton and main logic

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,34 @@
+class GroupsController < ApplicationController
+    before_action :authenticate_user!
+
+    def new
+        @group = Group.new()
+    end
+    
+    def create
+        
+        @group = Group.new(params.require(:group).permit(:name))     
+        
+        @user = current_user   
+        @group.users << @user
+        email_params["emails"].split(",").each do |e|
+            if !@group.users.collect { |p| "#{p[:email]}" }.include?(e)
+                @group.users << User.where(email: e)
+            end
+        end
+        
+        if @group.save
+            redirect_to root_path     
+        end
+    
+    end
+    
+    def email_params
+        params.require(:group).permit(:emails)
+    end
+
+    def show
+        @group = Group.find_by_id(params[:id])
+        @expenses = Expense.where(group_id: @group.id)
+    end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+ User.create! :email => 'john@educative.io', :password => 'topsecret', :password_confirmation => 'topsecret';
+ User.create! :email => 'max@educative.io', :password => 'topsecret', :password_confirmation => 'topsecret';
+ User.create! :email => 'della@educative.io', :password => 'topsecret', :password_confirmation => 'topsecret';
