868f61b57c6ab8eaed9ab4355e9c87fb8ab0506f
yury.sadouski <yury.sadouski@softswiss.com>
2024-12-01 03:37:47 +0300

    Updated styles for all forms, fixed navigation to home cresnn from exepcnsive

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
- User.create! :email => 'john@educative.io', :password => 'topsecret', :password_confirmation => 'topsecret';
- User.create! :email => 'max@educative.io', :password => 'topsecret', :password_confirmation => 'topsecret';
- User.create! :email => 'della@educative.io', :password => 'topsecret', :password_confirmation => 'topsecret';
+ User.create! :email => 'yury.sadouski@gmail.com', :password => 'password', :password_confirmation => 'password';
+ User.create! :email => 'kseni.sadouski@gmail.com', :password => 'password', :password_confirmation => 'password';
+ User.create! :email => 'della@gmail.com', :password => 'topsecret', :password_confirmation => 'topsecret';
